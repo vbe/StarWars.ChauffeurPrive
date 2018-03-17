aa66d586b1f51a53549ce66577a12653bcd4cd31
vincent <vbessuges@gmail.com>
2018-03-17 07:43:01 +0100

    Add TripListActivity

---
+++ app/src/main/java/fr/bessugesv/starwarschauffeurprive/app/triplist/activity/TripListActivity.kt
@@ -0,0 +1,9 @@
+package fr.bessugesv.starwarschauffeurprive.app.triplist.activity
+
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by Vincent on 3/17/2018.
+ */
+class TripListActivity : AppCompatActivity() {
+}
