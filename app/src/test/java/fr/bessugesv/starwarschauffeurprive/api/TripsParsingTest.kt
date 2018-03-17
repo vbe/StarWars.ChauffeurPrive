a8574e843d7ab46d64d60b9913b4a920e1bf245f
vincent <vbessuges@gmail.com>
2018-03-17 22:10:53 +0100

    Add tests for parsing of trip list API data

--- app/src/test/java/fr/bessugesv/starwarschauffeurprive/api/TripsParsingTest.kt
+++ app/src/test/java/fr/bessugesv/starwarschauffeurprive/api/TripsParsingTest.kt
@@ -6,6 +6,7 @@ import com.squareup.okhttp.mockwebserver.MockWebServer
 import fr.bessugesv.starwarschauffeurprive.model.Trip
 import okio.Buffer
 import org.hamcrest.Matchers.equalTo
+import org.hamcrest.Matchers.notNullValue
 import org.junit.Assert.assertThat
 import org.junit.Before
 import org.junit.Test
@@ -30,11 +31,40 @@ class TripsParsingTest {
     }
 
     @Test
-    fun testDarkVador() {
+    fun testTrip1ContainsData() {
+        val trip1 = trips[0]
+
+        assertThat(trip1.id, equalTo(1L))
+        assertThat(trip1.duration, equalTo(19427000L))
+        assertThat(trip1.pilot, notNullValue())
+        assertThat(trip1.distance, notNullValue())
+        assertThat(trip1.pickUp, notNullValue())
+        assertThat(trip1.dropOff, notNullValue())
+    }
+
+    @Test
+    fun testTrip1Pilot() {
         val darkVador = trips[0].pilot!!
 
         assertThat(darkVador.name, equalTo("Dark Vador"))
         assertThat(darkVador.avatarPath, equalTo("/static/dark-vador.png"))
         assertThat(darkVador.rating, equalTo(5.0f))
+    }
+
+    @Test
+    fun testTrip1Distance() {
+        val distance = trips[0].distance!!
+
+        assertThat(distance.unit, equalTo("km"))
+        assertThat(distance.value, equalTo(2478572L))
+    }
+
+    @Test
+    fun testTrip1DropOff() {
+        val dropOff = trips[0].dropOff!!
+
+        assertThat(dropOff.name, equalTo("Naboo"))
+        assertThat(dropOff.picturePath, equalTo("/static/naboo.png"))
+        assertThat(dropOff.date, equalTo("2017-12-09T19:35:51Z"))
     }
 }
