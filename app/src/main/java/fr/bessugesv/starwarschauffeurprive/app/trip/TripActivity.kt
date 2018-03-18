e75042bd605f6c061e8b88ebdb801cf609b2e37b
vincent <vbessuges@gmail.com>
2018-03-18 20:53:53 +0100

    Add toolbar on trop screen

--- app/src/main/java/fr/bessugesv/starwarschauffeurprive/app/trip/TripActivity.kt
+++ app/src/main/java/fr/bessugesv/starwarschauffeurprive/app/trip/TripActivity.kt
@@ -6,7 +6,6 @@ import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import android.widget.TextView
 import com.bumptech.glide.Glide
 import fr.bessugesv.starwarschauffeurprive.R
 import fr.bessugesv.starwarschauffeurprive.api.StarWarsApi
@@ -23,9 +22,6 @@ import java.util.*
  */
 class TripActivity : AppCompatActivity()  {
 
-
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -36,6 +32,13 @@ class TripActivity : AppCompatActivity()  {
         }
 
         val binding = DataBindingUtil.setContentView<PageTripBinding>(this, R.layout.page_trip)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.let {
+            it.setDisplayHomeAsUpEnabled(true)
+            it.setDisplayShowHomeEnabled(true)
+            it.title = ""
+        }
 
         StarWarsApi.service.trip(tripId).enqueue(object : Callback<Trip> {
             override fun onResponse(call: Call<Trip>?, response: Response<Trip>?) {
@@ -70,11 +73,10 @@ class TripActivity : AppCompatActivity()  {
 
     }
 
-
-
-
-
-
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
 
     companion object {
         const val EXTRA_TRIP_ID = "EXTRA_TRIP_ID"
